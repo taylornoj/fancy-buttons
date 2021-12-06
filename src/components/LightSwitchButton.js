aee8cac154f383d92e788ea95e868d4357eb404c
Taylor Noj <taylornoj@gmail.com>
2021-12-06 15:34:43 -0500

    better use of states and project complete

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -3,12 +3,12 @@ import { useState } from "react";
 
 
 function LightSwitchButton(props){
-  const {light, setLight} = props;
+  const {light, switchLight} = props;
 
-  const handleClick = () => setLight(light === "on" ? "off" : "on");
+  const handleClick = () => switchLight();
   
   return (
-    <button onClick={handleClick} className="LightSwitchButton">
+    <button onClick={switchLight} className="LightSwitchButton">
       {light === "on" && <span><i>💡</i> I'm on!</span>}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
