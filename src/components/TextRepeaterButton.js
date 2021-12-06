b8abead55672eab224d90fb93a8acf14390b42eb
Taylor Noj <taylornoj@gmail.com>
2021-12-06 08:11:19 -0500

    set up files for project and add components

---
+++ src/components/TextRepeaterButton.js
@@ -0,0 +1,11 @@
+import React from 'react'; //optional
+
+function TextRepeaterButton(){
+  return (
+    <button className="TextRepeaterButton">
+      <span>I like this text</span>
+    </button>
+  );
+}
+
+export default TextRepeaterButton;
