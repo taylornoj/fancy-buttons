29eec6cf0808c25f97b9664a3362ad998f1998e9
Taylor Noj <taylornoj@gmail.com>
2021-12-06 12:52:48 -0500

    modify the state of components

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -4,8 +4,11 @@ import { useState } from "react";
 
 function LightSwitchButton(props){
   const [light, setLight] = useState("off");
+
+  const handleClick = () => setLight(light === "on" ? "off" : "on");
+  
   return (
-    <button className="LightSwitchButton">
+    <button onClick={handleClick} className="LightSwitchButton">
       {light === "on" && <span className="on"><i>💡</i> I'm on!</span>}
       {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
     </button>
